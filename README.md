8a50c99959ab3fb1c8c6270bda2ddd4a61e6f9f3
Jeff Matheson <jeff.matheson@tetratech.com>
2024-05-04 17:02:44 -0700

    data processing

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# Birdnet Analysis
+# Acoustic Analysis Using BirdNET-Analyzer and Other Tools
 
-Use of the BirdNET detector and tools to analyze acoustic data. 
+Use of the BirdNET-Analyzer detector and other tools to analyze acoustic data. 
 
 
 BirdNET-Analyzer is a python mode/tool. Can be erun from command is Windows shell, in a python programming environment (e.g., VS Code), also within R using the NSNSDAcoustics package. In R, it still requires that Python be installed. 
 
-
+NSNSDAAcoustics has excellent help in the main repo and the R help files.
+<https://github.com/nationalparkservice/NSNSDAcoustics>
--- process_data.R
+++ process_data.R
@@ -2,6 +2,9 @@
 
 
 library(NSNSDAcoustics)
+library(tidyverse)
+library(hms)
+
 
 # Directory for birdnet result files. All in one folder.
 dir_birdnet_out <- "C:/Users/jeff.matheson/audio_files/Squamish/birdnet_out"
@@ -15,3 +18,25 @@ formatted.results <- birdnet_gather(
   results.directory = dir_birdnet_out,
   formatted = TRUE
 )
+
+
+# Create Site ID and date/time from the filename.
+
+
+formatted.results <-
+  formatted.results |>
+  separate_wider_delim(
+    recordingID,
+    "_",
+    names = c("site_id", "date", "time"),
+    cols_remove = FALSE,
+    too_many = "drop")
+
+formatted.results$time <- str_sub(formatted.results$time, 1, 6)
+formatted.results$date <- ymd(formatted.results$date)
+formatted.results$time <- as_hms(parse_date_time(formatted.results$time, "HMS"))
+
+# Summarize Data
+
+
+
